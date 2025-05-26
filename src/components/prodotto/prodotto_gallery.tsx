675201ef44fb7c4a9071b4e80a1e79cfa3bd63c0
Manuel De Ceglie <amoonpod@gmail.com>
2025-05-26 11:54:59 +0200

    improve product gallery

--- src/components/prodotto/prodotto_gallery.tsx
+++ src/components/prodotto/prodotto_gallery.tsx
@@ -10,9 +10,9 @@ export default function ProdottoGallery({ images }: { images: Immagine[] }) {
   const [imgIndex, setImgIndex] = useState(0);
   const [isMobile, setIsMobile] = useState(false);
 
-  // Check if the screen is mobile
+  // Check if the screen is mobile - align with CSS breakpoint
   useEffect(() => {
-    const handleResize = () => setIsMobile(window.innerWidth < 640);
+    const handleResize = () => setIsMobile(window.innerWidth < 880);
     handleResize(); // check on mount
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
@@ -31,42 +31,47 @@ export default function ProdottoGallery({ images }: { images: Immagine[] }) {
   };
 
   return (
-    <div className="flex-1 w-full max-w-[500px]">
-      <div
-        className="cursor-pointer mb-4 flex justify-center"
-        onClick={() => setShowLightbox(true)}
-      >
-        <img
-          src={images[imgIndex].image}
-          alt="gallery-cover-image"
-          className="aspect-square object-cover w-full sm:w-96 rounded-2xl"
-        />
-      </div>
-
-      <ul
-        className={`${styles.productThumbnails} ${utils.flex} justify-center`}
-      >
-        {images.map((element: Immagine, index: number) => (
-          <li
-            key={`thumbnail-${index}`}
-            className={
-              imgIndex === index
-                ? `${styles.productThumbnail} ${styles.productThumbnailActive}`
-                : styles.productThumbnail
-            }
-            onClick={() => setImgIndex(index)}
+    <div className="flex-1 w-full max-w-[500px] relative">
+      {/* Desktop/Tablet Gallery */}
+      {!isMobile && (
+        <>
+          <div
+            className="cursor-pointer mb-4 flex justify-center"
+            onClick={() => setShowLightbox(true)}
           >
             <img
-              src={element.image}
-              alt={`product thumbnail ${index}`}
-              className="aspect-square object-cover w-16 h-16 sm:w-24 sm:h-24 rounded"
+              src={images[imgIndex].image}
+              alt="gallery-cover-image"
+              className="aspect-square object-cover w-full sm:w-96 rounded-2xl"
             />
-          </li>
-        ))}
-      </ul>
+          </div>
 
-      {/* Conditionally render slider only on non-mobile devices */}
-      {!isMobile && (
+          <ul
+            className={`${styles.productThumbnails} ${utils.flex} justify-center`}
+          >
+            {images.map((element: Immagine, index: number) => (
+              <li
+                key={`thumbnail-${index}`}
+                className={
+                  imgIndex === index
+                    ? `${styles.productThumbnail} ${styles.productThumbnailActive}`
+                    : styles.productThumbnail
+                }
+                onClick={() => setImgIndex(index)}
+              >
+                <img
+                  src={element.image}
+                  alt={`product thumbnail ${index}`}
+                  className="aspect-square object-cover w-16 h-16 sm:w-24 sm:h-24 rounded"
+                />
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
+
+      {/* Mobile Slider */}
+      {isMobile && (
         <>
           <div
             className={styles.productGallerySlider}
@@ -77,31 +82,37 @@ export default function ProdottoGallery({ images }: { images: Immagine[] }) {
                 key={`slider-${index}`}
                 src={element.image}
                 alt={`Product Image ${index}`}
+                className="aspect-square object-cover w-full rounded-2xl flex-shrink-0"
               />
             ))}
           </div>
-          <button className={styles.btnSliderPrev} onClick={goToPrevSlide}>
-            <svg viewBox="0 0 12 18" xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M11 1 3 9l8 8"
-                stroke="#1D2026"
-                strokeWidth="3"
-                fill="none"
-                fillRule="evenodd"
-              />
-            </svg>
-          </button>
-          <button className={styles.btnSliderNext} onClick={goToNextSlide}>
-            <svg viewBox="0 0 13 18" xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="m2 1 8 8-8 8"
-                stroke="#1D2026"
-                strokeWidth="3"
-                fill="none"
-                fillRule="evenodd"
-              />
-            </svg>
-          </button>
+
+          {images.length > 1 && (
+            <>
+              <button className={styles.btnSliderPrev} onClick={goToPrevSlide}>
+                <svg viewBox="0 0 12 18" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M11 1 3 9l8 8"
+                    stroke="#1D2026"
+                    strokeWidth="3"
+                    fill="none"
+                    fillRule="evenodd"
+                  />
+                </svg>
+              </button>
+              <button className={styles.btnSliderNext} onClick={goToNextSlide}>
+                <svg viewBox="0 0 13 18" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="m2 1 8 8-8 8"
+                    stroke="#1D2026"
+                    strokeWidth="3"
+                    fill="none"
+                    fillRule="evenodd"
+                  />
+                </svg>
+              </button>
+            </>
+          )}
         </>
       )}
     </div>

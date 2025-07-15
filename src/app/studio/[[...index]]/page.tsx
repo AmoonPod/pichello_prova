8d01a19a727b9673b8b6f991c8d39f47c1af761c
Manuel De Ceglie <amoonpod@gmail.com>
2025-07-15 08:41:03 +0200

    improvements for SEO

--- src/app/studio/[[...index]]/page.tsx
+++ src/app/studio/[[...index]]/page.tsx
@@ -2,6 +2,16 @@
 
 import { NextStudio } from "next-sanity/studio";
 import config from "../../../../sanity.config";
+import { Metadata } from "next";
+
+// Explicitly prevent indexing of the studio page
+export const metadata: Metadata = {
+  title: "Studio - Il Pichello",
+  robots: {
+    index: false,
+    follow: false,
+  },
+};
 
 export default function Studio() {
   return <NextStudio config={config} />;
